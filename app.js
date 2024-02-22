0c16aa98133b4141b2e3bcadb70e54d36b6baf0a
GARVIT SHARMA <154793464+techgarvit@users.noreply.github.com>
2024-02-22 22:14:37 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+document.write('<img src="/image/bg1.jpg" alt="banner" id="bg" height="250px" width="31%">')
+
+            document.write('<img src="/image/Screenshot 2024-02-22 082705.png" alt="banner" id="bg1" height="150px" width="150px">')
+  
+
+            document.write('<h2  id="head">NAME:&nbsp;XYZ<br><br>Contact:&nbsp;8272**9286<br><br>EMAIL:nbsp;xyz@gmail.com</h2>')
